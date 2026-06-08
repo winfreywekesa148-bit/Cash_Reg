8008ca4fc1924570623f5e523da045a8d8e5b91c
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-08 22:05:24 +0300

    Add a function for total, items.

--- cash_reg.py
+++ cash_reg.py
@@ -2,14 +2,24 @@
 class CashRegister:
     def __init__(self, discount):
         self.discount = discount
-        
-        self.discount = input("Enter discount percentage: ")
 
         if self.discount:
                 self.discount = int(self.discount)
         else:
                 self.discount = 0
 
-        pass
-    pass
+    def total(self):
+          return self.total_amount
     
+    def item(self):
+          items = []
+          return items
+
+    def previous_transaction(self):
+          previous_transactions = []
+          return previous_transactions
+
+
+
+
+discount = int(input("Enter discount percentage: "))
