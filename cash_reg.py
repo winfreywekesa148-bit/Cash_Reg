6778c05db308ab3a2554cb2053aa3152c496218c
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-08 22:54:38 +0300

    add items, price and items in a list

--- cash_reg.py
+++ cash_reg.py
@@ -2,24 +2,52 @@
 class CashRegister:
     def __init__(self, discount):
         self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
 
-        if self.discount:
-                self.discount = int(self.discount)
-        else:
-                self.discount = 0
+    @property
+    def discount(self):
+          return self._discount
+    
+    @discount.setter
+    def discount(self, value):
+          
+          if value >= 0 and value <= 100:
+                self._discount = value
+          else:
+                raise TypeError("Not valid discount")
+   
 
-    def total(self):
-          return self.total_amount
+    def totals(self):
+        return self._total
     
     def item(self):
-          items = []
-          return items
+          return self._items
 
     def previous_transaction(self):
-          previous_transactions = []
-          return previous_transactions
+          return self._previous_transactions
+    
+    #method
+    def add_item(self, item, price, quantity):
+         self.total.append(price)
+         self.total += price
 
+         self.items.append(item)
 
+         transaction = {
+              "item" : item,
+              "price" : price,
+              "quantity" : quantity
+         }
 
+         self.previous_transaction.append(transaction)
+    
 
+    
+
+    
 discount = int(input("Enter discount percentage: "))
+cash_register = CashRegister(discount)
+
+print(cash_register.discount)
