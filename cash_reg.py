742563b8f7d5b6f98e459e041bbb35f51a50f4aa
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-06-08 20:51:07 +0300

    create cash register

--- cash_reg.py
+++ cash_reg.py
@@ -1,4 +1,15 @@
 #create cash reg
 class CashRegister:
+    def __init__(self, discount):
+        self.discount = discount
+        
+        self.discount = input("Enter discount percentage: ")
+
+        if self.discount:
+                self.discount = int(self.discount)
+        else:
+                self.discount = 0
+
+        pass
     pass
     
